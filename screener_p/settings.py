98b6f3169d4cd7a13dab73f0f6bdf8ca74a66bd1
Bramvlaere <90640700+Bramvlaere@users.noreply.github.com>
2022-04-19 18:06:20 -0400

    Update settings.py

--- screener_p/settings.py
+++ screener_p/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY = 'django-insecure-v3ma4i=yg-#c0d@lt)bh0)5ci@4_7f#kt43czaw!se!#fgsg^p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
